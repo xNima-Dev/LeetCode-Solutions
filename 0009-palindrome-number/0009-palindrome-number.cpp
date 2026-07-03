7881abf53bad77fc436df0fbba01ac28b46c0324
xNima-Dev <nimsarakavindhu@gmail.com>
2026-07-03 08:57:04 +0530

    Time: 0 ms (100%), Space: 8.5 MB (92.5%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        long reversed = 0;
+        int original = x;
+
+        while (x > 0) {
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x /= 10;
+        }
+
+        return original == reversed;
+    }
+};
