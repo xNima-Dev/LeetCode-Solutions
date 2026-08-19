bfa25e217a347589eca425b2f2aa19e2bc54cbf9
xNima-Dev <nimsarakavindhu@gmail.com>
2026-08-19 08:57:53 +0530

    Time: 7 ms (78.95%), Space: 60.2 MB (10.53%) - LeetHub

---
+++ 0147-insertion-sort-list/0147-insertion-sort-list.ts
@@ -0,0 +1,28 @@
+function insertionSortList(head: ListNode | null): ListNode | null {
+    if (!head || !head.next) {
+        return head;
+    }
+
+    const dummy = new ListNode(0, head);
+    let lastSorted = head;
+    let curr = head.next;
+
+    while (curr !== null) {
+        if (lastSorted.val <= curr.val) {
+            lastSorted = lastSorted.next;
+        } else {
+            let prev = dummy;
+            while (prev.next! && prev.next.val <= curr.val) {
+                prev = prev.next;
+            }
+
+            lastSorted.next = curr.next;
+            curr.next = prev.next;
+            prev.next = curr;
+        }
+
+        curr = lastSorted.next;
+    }
+
+    return dummy.next;
+}
